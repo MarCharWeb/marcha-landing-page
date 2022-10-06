5f08e7ab19fd63b20d34f4b8af62808f0375f19b
Hoang Minh Quan <108247146+MarCharWeb@users.noreply.github.com>
2022-10-06 20:41:41 +0700

    fix custom font

--- pages/_document.js
+++ pages/_document.js
@@ -4,34 +4,7 @@ export default function Document() {
   return (
     <Html>
       <Head>
-        <link
-            rel="preload"
-            href="../fonts/Agrandir-Regular.otf"
-            as="font"
-            type="font/otf"
-            crossOrigin
-            />
-        <link
-            rel="preload"
-            href=".../fonts/Agrandir-TextBold.otf"
-            as="font"
-            type="font/otf"
-            crossOrigin
-            />
-        <link
-            rel="preload"
-            href="../fonts/Agrandir-GrandHeavy.otf"
-            as="font"
-            type="font/otf"
-            crossOrigin
-            />
-        <link
-            rel="preload"
-            href=".../fonts/Agrandir-GrandLight.otf"
-            as="font"
-            type="font/otf"
-            crossOrigin
-            />
+        
       </Head>
       <body>
         <Main />
