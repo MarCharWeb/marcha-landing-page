fe16262f990e26c9a0e12fef859ac55b45c280b8
Hoang Minh Quan <108247146+MarCharWeb@users.noreply.github.com>
2022-10-06 21:06:49 +0700

    fix hero background - 2

--- components/Hero.js
+++ components/Hero.js
@@ -1,8 +1,12 @@
 import ImageHolder from "./ImageHolder";
 import HeroBg2 from '../assets/hero-bg-2.png'
+import HeroBg1 from '../assets/hero-bg-1.png'
+
 
 const Hero = ({children}) => {
-return ( <header className="relative grid grid-cols-1 col-span-1 grid-row-1 row-span-1 z-[10] w-full hero-bg xl:min-h-[1440px] 2xl:min-h-[2000px] h-[1000px] bg-center	bg-cover bg-no-repeat   ">
+return ( <header style={{
+      backgroundImage: `url(${HeroBg1.src})`
+    }} className="relative grid grid-cols-1 col-span-1 grid-row-1 row-span-1 z-[10] w-full hero-bg xl:min-h-[1440px] 2xl:min-h-[2000px] h-[1000px] bg-center	bg-cover bg-no-repeat   ">
 
         <ImageHolder src={HeroBg2} priority={true} layout='fill' className='w-full h-[1000px] xl:h-[1440px] 2xl:h-[2000px] top-0 after:absolute after:inset-0 after:bg-gradient-to-b after:from-bg-500/50 after:to-bg-500/10 '  alt="marketing-challengers-background"></ImageHolder>
        
