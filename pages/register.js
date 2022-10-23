01ee00fe9f85db7d5b5be3b2e207e81a945ebf7c
Hoang Minh Quan <64230078+minhquan1701@users.noreply.github.com>
2022-10-23 17:56:19 +0700

    Update register.js

--- pages/register.js
+++ pages/register.js
@@ -298,8 +298,8 @@ const Register = () => {
                <PageTitle type={2} title='Individual Register'></PageTitle>
             <div  className="w-[95vw] xl:w-1/2 mx-auto mt-4 xl:mt-10 bg-bg-50 rounded-2xl pt-6 md:pt-10 xl:pt-14 min-h-[850px]  md:min-h-[900px]">
                 {isSubmitted ? <div className="w-3/4 p-2 mx-auto"> 
-                  <p ref={successMessRef}  className='mb-2 font-bold rounded text-headline-21 text-success-900'>Successfully registered! A confirmation email with Round 1 information will be sent to you shortly (please check your Inbox and Spam folder). If you have not received it within 1 hour, please contact us for assistance. <br/> You have also received a gift from our Diamond Sponsor - Fonos: <a target={'_blank'} className='italic underline hover:text-primary-500' rel="noreferrer" href='https://fonos.vn/marketing-challengers-11'>https://fonos.vn/marketing-challengers-11</a></p>
-                  <InternalLink></InternalLink> 
+                  <p ref={successMessRef}  className='mb-2 font-bold rounded text-headline-21 text-success-900'>Successfully registered! A confirmation email with Round 1 information will be sent to you shortly (please check your Inbox and Spam folder). If you have not received it within 1 hour, please contact us for assistance. <br/> Congratulations! By registering, you have also received a gift from our Diamond Sponsor - Fonos, which is an audiobook named “Tuần làm việc 4H”, valuing at 129,000 VND. Please click the following link to receive ❤️: <a target={'_blank'} className='italic underline hover:text-primary-500' rel="noreferrer" href='https://fonos.vn/marketing-challengers-11'>https://fonos.vn/marketing-challengers-11</a></p>
+                  <InternalLink className={'mt-4'}></InternalLink> 
                   </div> : 
                   <form className="w-10/12 mx-auto space-y-2 md:space-y-3 lg:space-y-5 md:w-2/3 text-headline-21 md:text-body-18 text-bg-500" onSubmit={formik.handleSubmit}>
                     <h2 ref={formTitleRef} className="font-bold text-primary-600 text-lead-24">Step {currentStep + 1}/{formStep.length}: {formStep[currentStep].stepDesc}</h2>
