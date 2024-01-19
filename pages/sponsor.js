170dfd01207af45c6dc2efeb8d49adf344b2f218
MarCharWeb <108247146+MarCharWeb@users.noreply.github.com>
2024-01-19 18:55:33 +0700

    change pages

---
+++ pages/sponsor.js
@@ -0,0 +1,60 @@
+import { useState } from "react";
+import SponsorDisplay from "../components/SponsorDisplay";
+import { sponsorLogo } from "../assets/sponsorLogo";
+import PageTitle from "../components/PageTitle";
+import Birdbg from "../assets/bird-bg.png"
+import SponsorTag from "../components/SponsorTag";
+import Footer from "../components/Footer";
+import Avabg from '../assets/Ava-background.png'
+
+
+const Sponsor = () => {
+
+    const [sponsorData, setSponsorData] = useState(sponsorLogo());
+    const handleTagClick = (sponsorYear) => {
+        let updateSponsorData = sponsorData.map(sps => {
+            if (sps.year === sponsorYear) {
+                return { ...sps, isActive: true };
+            } else {
+                return { ...sps, isActive: false };
+            }
+        })
+
+        setSponsorData(updateSponsorData)
+    }
+    return (<section className="py-10 relative z-[1] xl:py-16" style={{
+        backgroundImage: `url(${Avabg.src})`,
+        backgroundRepeat: 'no-repeat',
+        backgroundSize: 'cover',
+        backgroundPosition: '0% 0%',
+        zIndex: 2,
+    }}>
+        <PageTitle className="mt-10" title={'SPONSORSHIP'}></PageTitle>
+        {/* Sponsor Tag List */}
+
+        <div className="flex flex-wrap justify-center w-11/12 gap-4 pt-4 pb-8 mx-auto xl:w-10/12 xl:pt-6 xl:gap-6 2xl:w-8/12"
+            style={{
+                backgroundImage: `url(${Birdbg.src})`,
+                backgroundRepeat: 'no-repeat',
+                backgroundSize: '10% 60%',
+                backgroundPosition: '100% 105%',
+                zIndex: 1,
+            }
+            }>
+            {sponsorData.map((sps, index) => <SponsorTag key={index} handleTagClick={handleTagClick} text={sps.year} isActive={sps.isActive} />)}
+        </div>
+
+        <div className="w-10/12 mx-auto space-y-2.5 2xl:w-8/12">
+
+            {sponsorData.find(sps => sps.isActive).sponsorLogos.length > 0 ? sponsorData.find(sps => sps.isActive).sponsorLogos?.map((logoCategory, index) => <SponsorDisplay key={index} categoryName={logoCategory.categoryName} logoList={logoCategory.logoList} />)
+                :
+                <p className="text-center">To be updated</p>
+            }
+
+        </div>
+        <Footer></Footer>
+
+    </section>);
+}
+
+export default Sponsor;
