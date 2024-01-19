e5e517bdb2c6ad7e967be2e05a0c745ecd14a74f
MarCharWeb <108247146+MarCharWeb@users.noreply.github.com>
2024-01-20 03:54:56 +0700

    ss11

--- components/SponsorDisplay.js
+++ components/SponsorDisplay.js
@@ -2,10 +2,11 @@ import ImageHolder from '../components/ImageHolder'
 const SponsorDisplay = ({categoryName, logoList}) => {
     return ( 
         <div>
-            <div className="flex flex-col items-center justify-between ">
-                <h5 className='font-bold text-lead-24 xl:text-lead-32 lg:text-headline-32 text-glow-strong'>{categoryName}</h5>
+            <div className="flex flex-col items-center justify-between lg:flex-row">
+                <h5 className='font-bold text-lead-24 xl:text-lead-24 lg:text-headline-21'>{categoryName}</h5>
+            
                 <div className="max-w-[98%] xl:max-w-[80%] flex flex-wrap justify-center gap-5 items-center">
-                    {logoList.map((logo, index) => <a key={index} target="_blank" rel="noreferrer"   ><ImageHolder  objectFit='fill' className={(logo.hasOwnProperty("logoClassName") ? logo.logoClassName : 'w-20 md:w-28 xl:w-40') + ""}  src={logo.logoImg}></ImageHolder></a>)} 
+                    {logoList.map((logo, index) => <a key={index} target="_blank" rel="noreferrer"  href={logo.hasOwnProperty("logoLink") ? logo.logoLink: ""} ><ImageHolder  objectFit='fill' className={(logo.hasOwnProperty("logoClassName") ? logo.logoClassName : 'w-20 md:w-28 xl:w-40') + ""}  src={logo.logoImg}></ImageHolder></a>)} 
                 </div>
 
             
--- pages/rules.js
+++ pages/rules.js
@@ -1,54 +1,26 @@
 import PageTitle from "../components/PageTitle";
-import WhiteLight from '../assets/Logolight-white.png'
-import Value from "../components/Value";
-import Birdbg from "../assets/bird-bg.png"
-import Footer from "../components/Footer";
-import Avabg from '../assets/Ava-background.png'
 
 const Rules = () => {
-    return ( <main className="pt-20 container-media-padding" style={{
-        backgroundImage: `url(${Avabg.src})`,
-        backgroundRepeat: 'no-repeat',
-        backgroundSize: 'cover',
-        backgroundPosition: '0% 0%',
-        zIndex: 2,
-    }}>
-        <div id="rules&regulations"/>
-        <PageTitle title={'RULES AND REGULATIONS'} type={2}></PageTitle>
-        <section className="py-10 space-y-6"
-        style={{
-            backgroundImage: `url(${Birdbg.src})`,
-            backgroundRepeat: 'no-repeat',
-            backgroundSize: '250px 350px',
-            backgroundPosition: '100% -2%',
-            zIndex: '1',
-        }}>
+    return ( <main className="pt-20 container-media-padding">
+        <PageTitle title={'Rules & Regulations'} type={2}></PageTitle>
+        <section className="py-10 space-y-6">
             <div className="space-y-1">
-                <div className="flex items-center ">
-                    <Value className="value" icon={WhiteLight} iconClassName="w-[120px] h-[120px] xl:w-[120px] xl:h-[120px]" />
-                    <h4 className="font-bold uppercase text-primary-500 -pl-10">ENTRANT ELIGIBILITY</h4>
-                </div>
-                <div className="-pt-32">
+                <h4 className="font-bold uppercase text-primary-50">Entrant ELIGIBILITY</h4>
                 <ul className="pl-4 space-y-1 list-disc">
                     <li><p>Current university/college students from year <strong>01</strong> to year <strong>04</strong> who are living, studying and working in Vietnam or studying abroad but are Vietnamese students.</p></li>
                     <li><p>Members of organising teams and (sponsor) related parties are <strong>NOT</strong> allowed to join the competition.</p></li>
                     <li><p>Compulsory language used in the competition: <strong>100% English</strong></p></li>
                 </ul>
-                </div>
-                
             </div>
 
-            <div className="space-y-1 pt-5">
-                <div className="flex items-center">
-                    <Value className=" value" icon={WhiteLight} iconClassName="w-[120px] h-[120px] xl:w-[120px] xl:h-[120px]" />
-                    <h4 className="font-bold uppercase text-primary-500 ">COMPETITION STRUCTURE</h4>
-                </div>
+            <div className="space-y-1">
+                <h4 className="font-bold uppercase text-primary-50">COMPETITION STRUCTURE</h4>
                 <ul className="pl-4 space-y-1 list-disc">
                     
                     <li><p> <strong className="italic">Round 1:</strong> Candidates have to independently complete <strong>an INDIVIDUAL online test.</strong></p></li>
 
                     <li>
-                       <strong className="italic"> Round 2:</strong><ul className="pl-4 space-y-1 list-[circle]">
+                       <strong className="italic"> Round 2:</strong><ul className="pl-4 space-y-1 list-[square]">
                             <li>
                                 Each individual who successfully passes round 1 has to register for a team. Each team includes 03 to 04 members. 
                             </li>
@@ -62,12 +34,12 @@ const Rules = () => {
                     </li>
 
                     <li>
-                       <strong className="italic"> Round 3:</strong><ul className="pl-4 space-y-1 list-[circle]">
+                       <strong className="italic"> Round 3:</strong><ul className="pl-4 space-y-1 list-[square]">
                             <li>
                                 TOP 16 will compete to pick out the best <strong>4 teams.</strong> 
                             </li>
                             <li>
-                                TOP 4 will present their final product at the Closing Ceremony. Teams that have members outside of Ho Chi Minh City will be pitching in hybrid mode.
+                                TOP 4 will present their final product at the Closing Ceremony. Teams that have members outside of Ho Chi Minh City will be pitching in hybrid mode 
                             </li>
                             
                         </ul>
@@ -76,30 +48,29 @@ const Rules = () => {
             </div>
 
 
-            <div className="space-y-1 pt-5">
-                <div className="flex items-center ">
-                    <Value className=" value" icon={WhiteLight} iconClassName="w-[120px] h-[120px] xl:w-[120px] xl:h-[120px]" />
-                    <h4 className="font-bold uppercase text-primary-500 ">REGISTRATION TIMELINE</h4>
-                </div>
+            <div className="space-y-1">
+                <h4 className="font-bold uppercase text-primary-50">REGISTRATION TIMELINE</h4>
                 <ul className="pl-4 space-y-1 list-disc">
                     
                     <li><p>From <strong className="text-primary-500">October 24 to November 14</strong></p></li>
 
                 </ul>
+            </div>
 
-                <h4 className="font-bold ">CONDITIONS & RULES FOR EVENT PARTICIPATION</h4>
+            <div className="space-y-1">
+                <h4 className="font-bold uppercase text-primary-50">CONDITIONS & RULES FOR EVENT PARTICIPATION</h4>
                 <p className="font-bold">Workshops:</p>
                 <ul className="pl-4 space-y-1 list-disc">
                     
-                    <li><strong className="">Condition to join:</strong>
-                        <ul className="pl-4 space-y-1 list-[circle]">
+                    <li><strong className="italic">Condition to join:</strong>
+                        <ul className="space-y-1">
                             <li>For Workshop 1: candidates who have successfully made it to Round 2.</li>
                             <li>For Workshop 2: candidates who have successfully made it to TOP 16.</li>
                         </ul>
                         
                     </li>
-                    <li><strong className="">Regulation for candidates:</strong>
-                        <ul className="pl-4 space-y-1 list-[circle]">
+                    <li><strong className="italic">Regulation for candidates:</strong>
+                        <ul className="pl-4 space-y-1 list-[square]">
                             <li>Join the event on time</li>
                             <li>Open the camera to communicate with guest speakers</li>
                             <li>Must not share screen or turn on the microphone while guest speakers are sharing</li>
@@ -114,13 +85,10 @@ const Rules = () => {
                 <p>Marketing Challengers still has live broadcasts of this series on Facebook Group.</p>
             </div>
 
-            <div className="space-y-1 pt-5">
-                <div className="flex items-center">
-                    <Value className=" value" icon={WhiteLight} iconClassName="w-[120px] h-[120px] xl:w-[120px] xl:h-[120px]" />
-                    <h4 className="font-bold uppercase text-primary-500 ">SUBMISSION GUIDELINE</h4>
-                </div>
+            <div className="space-y-1">
+                <h4 className="font-bold uppercase text-primary-50">SUBMISSION GUIDELINE</h4>
                 
-                <ul className="pl-4 space-y-1 list-disc">
+                <ul className="space-y-1">
                    <li><p>Follow the instructions of the organisers. Contact as soon as technical problems occur.</p></li> 
                    <li><p>Candidates are encouraged not to submit the test too close to the deadline to avoid unnecessary technical problems.</p></li> 
                    <li><p>Candidates who submit assignments late or incorrectly will be disqualified.</p></li> 
@@ -128,12 +96,10 @@ const Rules = () => {
                 </ul>
                 
             </div>
-            <div className="space-y-1 pt-5">
-                <div className="flex items-center">
-                    <Value className=" value" icon={WhiteLight} iconClassName="w-[120px] h-[120px] xl:w-[120px] xl:h-[120px]" />
-                    <h4 className="font-bold uppercase text-primary-500 ">TERM & CONDITIONS</h4>
-                </div>                
-                <ul className="pl-4 space-y-1 list-disc">
+            <div className="space-y-1">
+                <h4 className="font-bold uppercase text-primary-50">TERM & CONDITIONS</h4>
+                
+                <ul className="space-y-1">
                    <li><p>No registration fees necessary for entering the competition.</p></li> 
                    <li><p>All materials submitted must be candidates’ own work and must not be under the submission of other competitions. Any duplication of outside sources must be referenced properly to avoid plagiarism.</p></li> 
                    <li><p>Candidates must not reveal the datasets and questions from all rounds to ensure the confidentiality of the contest.</p></li> 
@@ -147,13 +113,10 @@ const Rules = () => {
                 </ul>
                 
             </div>
-            <div className="space-y-1 pt-5">
-                <div className="flex items-center">
-                    <Value className=" value" icon={WhiteLight} iconClassName="w-[120px] h-[120px] xl:w-[120px] xl:h-[120px]" />
-                    <h4 className="font-bold uppercase text-primary-500 ">COPYRIGHTS OF MARKETING CHALLENGER</h4>
-                </div>    
+            <div className="space-y-1">
+                <h4 className="font-bold uppercase text-primary-50">COPYRIGHTS OF MARKETING CHALLENGERS</h4>
                 
-                <ul className="pl-4 space-y-1 list-disc">
+                <ul className="space-y-1">
                    <li><p>Candidates must obey the copyright laws and not violate the copyrights of any other third parties. The Organisers of this Contest, RMIT Business Club, is not responsible for any breach by the candidate.</p></li> 
                    <li><p>RMIT Business Club reserves the right to publish and display the Project, using its content and images for publicity, promotion and non-payment purposes.</p></li> 
                    <li><p>The Organising Team, RMIT Vietnam Business Club, sponsors and RMIT will not use the candidate&apos; s work for any purpose other than grading and filtering out the best works for awarding.</p></li> 
@@ -163,10 +126,9 @@ const Rules = () => {
                 </ul>
                 
             </div>
+            
         </section>
-        <Footer></Footer>
-    </main> 
-    );
+    </main> );
 }
  
 export default Rules;
