6435ecb87735fecb525ff9e5c9d22f8276aa038b
MarCharWeb <108247146+MarCharWeb@users.noreply.github.com>
2024-01-26 19:54:17 +0700

    Update ourhuman.js

--- pages/ourhuman.js
+++ pages/ourhuman.js
@@ -1,12 +1,12 @@
 import PageTitle from "../components/PageTitle";
 import Image from 'next/image';
 import Footer from "../components/Footer";
-import Leaderboardimg from '../assets/TeamImage/Leaderboard.jpg';
- import Marketingimg from '../assets/TeamImage/Marketing-Team.jpg';
- import Mediaimg from '../assets/TeamImage/Media-Team.jpg';
- import Operationsimg from '../assets/TeamImage/Operations-Team.jpg';
- import Programimg from '../assets/TeamImage/Program-Team.jpg';
- import Sponsorimg from '../assets/TeamImage/Team-Sponsor.jpg';
+// import Leaderboardimg from '../assets/TeamImage/Leaderboard.jpg';
+//  import Marketingimg from '../assets/TeamImage/Marketing-Team.jpg';
+//  import Mediaimg from '../assets/TeamImage/Media-Team.jpg';
+//  import Operationsimg from '../assets/TeamImage/Operations-Team.jpg';
+//  import Programimg from '../assets/TeamImage/Program-Team.jpg';
+//  import Sponsorimg from '../assets/TeamImage/Team-Sponsor.jpg';
 
 import Avabg from '../assets/Ava-background.png';
 
