eeeaa61c9f32e9ccd9207ebc8ab1e4bbfe6e9550
MarCharWeb <108247146+MarCharWeb@users.noreply.github.com>
2024-02-03 19:01:07 +0700

    Update ourhuman.js

--- pages/ourhuman.js
+++ pages/ourhuman.js
@@ -32,7 +32,7 @@ const Ourhuman = () => {
                 <div className="lg:w-1/2 w-full bg-gray-300 ">
                     <h4 className="flex items-center justify-center font-bold text-headline-31 lg:text-headline-37 text-glow-strong">Sponsor Team</h4>
                       <div className="flex items-center justify-center">
-                        <Image className="flex items-center justify-center h-screen" src={Sponsorimg} width={650} height={390} />
+                        <Image className="flex items-center justify-center h-screen" src={Sponsorimg} width={620} height={330} />
                         </div>
                 </div>
                 <div className="lg:w-1/2 w-full bg-blue-300 text-justify mt-10 lg:mx-4 flex items-center justify-center">
