eaa3e396d837156b985bbd094b55825a5c1eedcd
MarCharWeb <108247146+MarCharWeb@users.noreply.github.com>
2024-02-29 02:31:42 +0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -4,12 +4,12 @@ import ImageHolder from '../components/ImageHolder'
 import PageTitle from '../components/PageTitle'
 import SectionTitle from '../components/SectionTitle'
 import AboutUsImg from '../assets/about-us.jpg'
-import Timeline from '../components/Timeline'
+// import Timeline from '../components/Timeline'
 import Award from '../components/Award'
 import Sponsor from '../components/Sponsor'
 import CallToAction from '../components/CallToAction'
 import ValueProps from '../components/ValueProps'
-import HeroText from '../assets/hero-text.png'
+import HeroText from '../assets/spark.png'
 import { sanityClient } from '../config/sanity';
 import { gsap } from "gsap";
 import { useLayoutEffect, useRef } from "react";
@@ -20,13 +20,58 @@ import Script from 'next/script'
 import Whocanjoin from '../components/Whocanjoin'
 import Avabg from '../assets/Ava-background.png'
 import Footer from "../components/Footer";
-import Leaderboardimg from '../assets/TeamImage/Leaderboard.jpg';
-import Marketingimg from '../assets/TeamImage/Marketing-Team.jpg';
-import Mediaimg from '../assets/TeamImage/Media-Team.jpg';
-import Operationsimg from '../assets/TeamImage/Operations-Team.jpg';
-import Programimg from '../assets/TeamImage/Program-Team.jpg';
-import Sponsorimg from '../assets/TeamImage/Team-Sponsor.jpg';
-import Ourhumanlp from '../components/Ourhuman-lp'
+import Ourhumanlp from '../components/Ourhuman-lp';
+import Awardstructure from '../components/awardstructure';
+import newbg from '../assets/newbg.png';
+import { useState, useEffect } from 'react';
+import Image from 'next/image';
+import LayerImage from '../assets/layer.png';
+
+
+
+const Countdown = ({ targetDate }) => {
+  const calculateTimeLeft = () => {
+    const difference = +new Date(targetDate) - +new Date();
+    let timeLeft = {};
+
+    if (difference > 0) {
+      timeLeft = {
+        Dates: Math.floor(difference / (1000 * 60 * 60 * 24)),
+        Hours: Math.floor((difference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)),
+        Minutes: Math.floor((difference % (1000 * 60 * 60)) / (1000 * 60)),
+        Seconds: Math.floor((difference % (1000 * 60)) / 1000),
+      };
+    }
+
+    return timeLeft;
+  };
+
+  const formatValue = (value) => {
+    return value < 10 ? `0${value}` : value;
+  };
+
+  const [timeLeft, setTimeLeft] = useState(calculateTimeLeft());
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setTimeLeft(calculateTimeLeft());
+    }, 1000);
+
+    return () => clearInterval(timer);
+  }, []);
+
+  return (
+    <div className="flex justify-center space-x-4">
+      {Object.entries(timeLeft).map(([unit, value]) => (
+        <div key={unit} className="text-center">
+          <div className="text-[100px] font-black">{formatValue(value)}</div>
+          <div className="text-sm">{unit}</div>
+        </div>
+      ))}
+    </div>
+  );
+};
+
 
 export default function Home({ data }) {
 
@@ -42,13 +87,15 @@ export default function Home({ data }) {
 
 
   return (
-    <main className='' style={{
-      backgroundImage: `url(${Avabg.src})`,
-      backgroundRepeat: 'no-repeat',
-      backgroundSize: 'cover',
-      backgroundPosition: '0%',
-      zIndex: '2',
-    }} >
+    <main className="" style={{
+      backgroundImage: `url(${LayerImage.src}), url(${newbg.src})`,
+      backgroundRepeat: 'no-repeat, no-repeat',
+      backgroundPosition: 'center -70px, 0% 0%',
+      backgroundSize: '100% auto, auto',  // Adjust as per your requirement
+      backgroundAttachment: 'scroll, fixed',
+    }}>
+      <div className="w-full">
+      </div>
       <Script src="https://www.googletagmanager.com/gtag/js?id=GA_MEASUREMENT_ID" />
       <Script id="google-analytics">
         {`
@@ -68,30 +115,55 @@ export default function Home({ data }) {
 
       </Head>
 
-      <Hero>
-        <div className='absolute flex flex-col pt-20 xl:pt-[150px] 2xl:pt-40 items-center w-full' style={{ zIndex: 2 }} >
+
+      <div className='absolute flex flex-col pt-20 xl:pt-[150px] 2xl:pt-40 items-center w-full' style={{ zIndex: 2 }} >
+        <div>
+          <div className='' 
+          // style={{
+          //   backgroundImage: `url(${LayerImage.src})`,
+          //   backgroundSize: '100% auto',
+          //   backgroundRepeat: 'no-repeat',
+          //   backgroundPosition: '0% 0%',
+          //   zIndex: 2,}}
+            >
+            {/* <PageTitle type={2} title='MARKETING CHALLENGERS'></PageTitle> */}
+            <ImageHolder ref={heroTextRef} priority={true} src={HeroText} alt="marketing-challengers-ss12-slogan" className={'w-56 h-56 lg:w-64 lg:h-64 xl:w-[583px] mx-auto xl:h-[583px] transform -translate-y-10 lg:-translate-y-18 xl:-translate-y-20 sm:pt-10'}></ImageHolder>          </div>
+          <PageTitle className='hidden lg:flex justify-center mb-6 pt-16' title='COUNTDOWN TO OPEN ROUND 1' />
           <div>
-            <PageTitle type={2} title='MARKETING CHALLENGERS'></PageTitle>
-            <ImageHolder ref={heroTextRef} priority={true} src={HeroText} alt="marketing-challengers-ss11-slogan" className={'w-56 h-56 lg:w-64 lg:h-64 xl:w-[583px] mx-auto xl:h-[583px] transform -translate-y-10 lg:-translate-y-18 xl:-translate-y-20 sm:pt-10'}></ImageHolder>
-            <PageTitle className='hidden lg:flex justify-center' title='MARKETING CHALLENGERS' />
-            <p className='hidden lg:flex text-center mx-auto p-8 font-bold text-headline-31 xl:w-2/3 pt-0 mt-6 sm:w-90%'>
-              Marketing Challengers is a top 3 national competition for Integrated Marketing Communications plans, organized by the RMIT Vietnam Business Club - SGS. With an impressive track record over a decade with 11 seasons, Marketing Challengers takes immense pride in the journey of inspiring and nurturing a vibrant community of marketers across Vietnam.
-            </p>
-
+            <div className='flex items-center justify-center text-[30px] lg:text-headline-30 2xl:text-hero-60 xl:text-[40px] tracking-widest leading-10 text-glow-strong box-border w-2/3 h-[200px] border-8 mx-auto pt-8'>
+              <Countdown targetDate={new Date('2024-04-04T20:00:00+07:00')} />
+            </div>
+            <div className='relative flex justify-center -top-16 lg:-top-2 xl:-top-2 sm:top-6 mt-8'>
+              <Button ref={heroBtn} onClick={() => {
+                // route.push('/aboutus')
+              }} isGlow={true} type='primary' className=' animate-bounce-slow font-bold' text={'REGISTER NOW'} size='large'></Button>
+            </div>
+            <div className='pl-4 space-y-0 lg:space-y-1'>
+              <p className='text-center text-headline-31 font-bold text-glow-strong'>More information:</p>
+            </div>
+            <div className='relative flex justify-center -top-16 lg:-top-2 xl:-top-2 sm:top-6 mt-4'>
+              <Button ref={heroBtn} onClick={() => {
+                // route.push('/aboutus')
+              }} isGlow={true} type='primary' className=' animate-bounce-slow font-bold' text={'Registration for Opening Ceremony'} size='large'></Button>
+            </div>
           </div>
-
-          <div className='relative flex justify-center -top-16 lg:-top-2 xl:-top-2 sm:top-6'>
-            <Button ref={heroBtn} onClick={() => { route.push('/aboutus') }} isGlow={true} type='primary' className=' animate-bounce-slow font-bold' text={'About Marketing Challengers'} size='large'></Button>
-          </div>
+          <PageTitle className='hidden lg:flex justify-center mt-20' title='MARKETING CHALLENGERS' />
+          <p className='hidden lg:flex mx-auto p-8 font-bold text-headline-31 xl:w-3/4 pt-0 mt-6 sm:w-095% text-justify'>
+            Marketing Challengers is a top 3 national competition for Integrated Marketing Communications plans, organized by the RMIT Vietnam Business Club - SGS. With an impressive track record over a decade with 11 seasons, Marketing Challengers takes immense pride in the journey of inspiring and nurturing a vibrant community of marketers across Vietnam.
+          </p>
 
         </div>
-      </Hero>
+        <div className='relative flex justify-center -top-16 lg:-top-2 xl:-top-2 sm:top-6'>
+          <Button ref={heroBtn} onClick={() => { route.push('/aboutus') }} isGlow={true} type='primary' className=' animate-bounce-slow font-bold' text={'About Marketing Challengers'} size='large'></Button>
+        </div>
 
-      <ValueProps></ValueProps>
+      </div>
+
+      {/* <ValueProps></ValueProps> */}
 
       {/* text-[30px] lg:text-headline-30 invisible hero-title 2xl:text-hero-60 xl:text-[60px] tracking-widest leading-10  lg:text-glow-strong  text-primary-500 */}
       {/* <Timeline></Timeline> */}
-
+      <Awardstructure></Awardstructure>
       <Sponsor></Sponsor>
 
 
