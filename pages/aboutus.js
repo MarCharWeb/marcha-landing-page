9b61dcddaa4f1259109d6c59c6a594483d75adaf
MarCharWeb <108247146+MarCharWeb@users.noreply.github.com>
2024-02-29 16:55:55 +0700

    Update aboutus.js

--- pages/aboutus.js
+++ pages/aboutus.js
@@ -53,36 +53,38 @@ const AboutUs = () => {
                         <div className='mt-40 '>
                             <PageTitle className="mt-64 hidden md:flex items-center justify-center" title={'SEASON 11 MILESTONES'}></PageTitle>
                         </div>
-                        <div  className="flex justify-between pt-8">
-                            <div  className='bg-transparent mx-4 group text-headline-31 relative w-[550px] py-10 border-2 border-yellow-500 rounded-md'>
+                        <div className="flex justify-center pt-8">
+                            <div className='bg-transparent mx-4 group text-headline-31 relative w-[400px] py-10 border-2 border-yellow-500 rounded-md'>
                                 <div className="text-center">
                                     <PageTitle title={'1100+'} className="pb-4"></PageTitle>
                                     <span className="font-bold text-[#FFFFFF] text-[40px] text-glow-strong">REGISTRATIONS</span>
                                 </div>
                             </div>
 
-                            <div  className='bg-transparent mx-4 group text-headline-31 relative w-[550px] py-10 border-2 border-yellow-500 rounded-md'>
+                            <div className='bg-transparent mx-4 group text-headline-31 relative w-[400px] py-10 border-2 border-yellow-500 rounded-md'>
                                 <div className="text-center">
                                     <PageTitle title={'210+'} className="pb-4"></PageTitle>
                                     <span className="font-bold text-[#FFFFFF] text-[40px] text-glow-strong">TEAMS</span>
                                 </div>
                             </div>
                         </div>
-                        <div className="flex justify-between py-8 ">
-                            <div  className='bg-transparent mx-4 group text-headline-31 relative w-[550px] py-10 border-2 border-yellow-500 rounded-md'>
+
+                        <div className="flex justify-center items-center py-8" >
+                        <div className='bg-transparent mx-4 group text-headline-31 relative w-[400px] py-10 border-2 border-yellow-500 rounded-md'>
                                 <div className="text-center">
                                     <PageTitle title={'30+'} className="pb-4"></PageTitle>
                                     <span className="font-bold text-[#FFFFFF] text-[40px] text-glow-strong">UNIVERSITIES</span>
                                 </div>
                             </div>
 
-                            <div  className='bg-transparent mx-4 group text-headline-31 relative w-[550px] py-10 border-2 border-yellow-500 rounded-md'>
+                            <div className='bg-transparent mx-4 group text-headline-31 relative w-[400px] py-10 border-2 border-yellow-500 rounded-md'>
                                 <div className="text-center">
                                     <PageTitle title={'31000+'} className="pb-4"></PageTitle>
                                     <span className="font-bold text-[#FFFFFF] text-[40px] text-glow-strong">FOLLOWERS</span>
                                 </div>
                             </div>
                         </div>
+
                     </div>
                 </div>
             </section>
