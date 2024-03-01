a42003fe5c15a73c9c0be89e22aec3679325a8e7
MarCharWeb <108247146+MarCharWeb@users.noreply.github.com>
2024-03-01 17:04:37 +0700

    Update awardstructure.js

--- components/awardstructure.js
+++ components/awardstructure.js
@@ -19,21 +19,27 @@ const Awardstructure = () => {
             <div className='bg-transparent group text-headline-31 relative w-[550px] py-10 border-2 border-yellow-500 rounded-md'>
                 <div className="text-center">
                     <PageTitle title={'CHAMPION'} className="pb-4"></PageTitle>
-                    <span className="font-bold text-[#FFFFFF] text-[40px] text-glow-strong">400,000,000 VND</span>
+                    <span className="font-bold text-[#FFFFFF] text-[40px] text-glow-strong">250,000,000 VND</span>
                 </div>
-                <span style={{ width: '625px', left: '50%', top: '50px', zIndex: '50', backgroundColor: '#3D067D', opacity: '0.9', zIndex: '50', fontFamily: 'Brandon Grotesque' }} className="absolute hidden group-hover:block bg-black text-white border-2 border-yellow-500 rounded-md p-5">
+                <span style={{ width: '720px', left: '30%', top: '50px', zIndex: '50', backgroundColor: '#3D067D', opacity: '0.9', zIndex: '50', fontFamily: 'Brandon Grotesque' }} className="absolute hidden group-hover:block bg-black text-white border-2 border-yellow-500 rounded-md px-5">
                     <div className="w-full flex justify-center">
                         <span className="font-black text-[18px] text-glow-strong">CHAMPION</span>
                     </div>
-                    <ul className="list-disc text-justify font-light text-[18px]">
-                        <li><p>1 Electric car + Internship opportunity from MET EV/person</p></li>
-                        <li><p>1 sampling box including lotion full size 8oz x2 + cream full size 120z x2 from Cerave/person</p></li>
-                        <li><p>1 3-month membership card from Fonos audiobook/person</p></li>
-                        <li><p>1 21-day experience voucher from Cafornia Fitness & Yoga/person</p></li>
-                        <li><p>1 Free 2-day 1-night ticket for resort room at MVillage branch/person</p></li>
-                        <li><p>Any one full course in the list of 13 courses from Brands Vietnam/person</p></li>
-                        <li><p>1 dental care package worth 1.5 million from Singae Dental/person</p></li>
-                        <li><p>Offer shsortcut to the interview round at the Marketing Trainee position at MVillage</p></li>
+                    <ul className="list-disc text-justify font-light text-[16px] leading-6">
+                        <li><p>1 electric motorbike from MET EV per person</p></li>
+                        <li><p>1 2-day 1-night ticket for resort room at a MVillage branch per person</p></li>
+                        <li><p>1 sampling box from Cerave per person</p></li>
+                        <li><p>1 L'Usine Signature Totebag per person</p></li>
+                        <li><p>1 2-day 1-night ticket for resort room at a MVillage branch per person</p></li>
+                        <li><p>1 three-month membership card from Fonos audiobooks per person</p></li>
+                        <li><p>1 21-day experience voucher from California Fitness & Yoga per person</p></li>
+                        <li><p>Study free 1 course from the list of 13 courses from Brands Vietnam per person</p></li>
+                        <li><p>1 dental care package from Singae Dental per person</p></li>
+                        <li><p>Three piece non-stick ceramic coated cookware set from Morandi Amercook AC.0106.03 per person</p></li>
+                        <li><p>1 box of sachet products consisting of Kaya Premium Hainanese and Kaya Premium Nonya per person</p></li>
+                        <li><p>1 internship opportunity from MET EV per person</p></li>
+                        <li><p>1 shortcut offer to interview round for Marketing Trainee position at MVillage per person</p></li>
+                        <li><p>1 shortcut offer to interview round for Executive position at Kaya House per person</p></li>
                     </ul>
                 </span>
             </div>
@@ -42,20 +48,24 @@ const Awardstructure = () => {
                 <div className='bg-transparent mx-4 group text-headline-31 relative w-[550px] py-10 border-2 border-yellow-500 rounded-md'>
                     <div className="text-center">
                         <PageTitle title={'1ST RUNNER UP'} className="pb-4"></PageTitle>
-                        <span className="font-bold text-[#FFFFFF] text-[40px] text-glow-strong">300,000,000 VND</span>
+                        <span className="font-bold text-[#FFFFFF] text-[40px] text-glow-strong">120,000,000 VND</span>
                     </div>
-                    <span style={{ width: '625px', left: '50%', top: '50px', zIndex: '50', backgroundColor: '#3D067D', opacity: '0.9', zIndex: '50', fontFamily: 'Brandon Grotesque' }} className="absolute hidden group-hover:block bg-black text-white border-2 border-yellow-500 rounded-md p-5">
+                    <span style={{ width: '720px', left: '50%', top: '50px', zIndex: '50', backgroundColor: '#3D067D', opacity: '0.9', zIndex: '50', fontFamily: 'Brandon Grotesque' }} className="absolute hidden group-hover:block bg-black text-white border-2 border-yellow-500 rounded-md p-5">
                         <div className="w-full flex justify-center">
                             <span className="font-black text-[18px] text-glow-strong">1ST RUNNER UP</span>
                         </div>
-                        <ul className="list-disc text-justify font-light text-[18px]">
-                            <li><p>Internship opportunitiy from MET EV/ person</p></li>
-                            <li><p>1 sampling box including lotion full size 8oz + cream full size 120z from Cerave/person</p></li>
-                            <li><p>1 3-month membership card from Fonos audiobook/person</p></li>
-                            <li><p>1 21-day experience voucher from Cafornia Fitness & Yoga/person</p></li>
-                            <li><p>Any one full course in the list of 13 courses from Brands Vietnam/person</p></li>
-                            <li><p>1 dental care package worth 1.5 million from Singae Dental/person</p></li>
-                            <li><p>Offer shsortcut to the interview round at the Marketing Trainee position at MVillage</p></li>
+                        <ul className="list-disc text-justify font-light text-[16px] leading-6">
+                            <li><p>1 sampling box from Cerave per person</p></li>
+                            <li><p>1 L'Usine Signature Totebag per person</p></li>
+                            <li><p>1 three-month membership card from Fonos audiobooks per person</p></li>
+                            <li><p>1 21-day experience voucher from California Fitness & Yoga per person</p></li>
+                            <li><p>Study free 1 course from the list of 13 courses from Brands Vietnam per person</p></li>
+                            <li><p>1 dental care package from Singae Dental per person</p></li>
+                            <li><p>Three piece non-stick ceramic coated cookware set from Morandi Amercook AC.0106.03 per person</p></li>
+                            <li><p>1 box of sachet products consisting of Kaya Premium Hainanese and Kaya Premium Nonya per person</p></li>
+                            <li><p>1 internship opportunity from MET EV per person</p></li>
+                            <li><p>1 shortcut offer to interview round for Marketing Trainee position at MVillage per person</p></li>
+                            <li><p>1 shortcut offer to interview round for Executive position at Kaya House per person</p></li>
                         </ul>
                     </span>
                 </div>
@@ -63,20 +73,21 @@ const Awardstructure = () => {
                 <div className='bg-transparent mx-4 group text-headline-31 relative w-[550px] py-10 border-2 border-yellow-500 rounded-md'>
                     <div className="text-center">
                         <PageTitle title={'2RD RUNNER UP'} className="pb-4"></PageTitle>
-                        <span className="font-bold text-[#FFFFFF] text-[40px] text-glow-strong">200,000,000 VND</span>
+                        <span className="font-bold text-[#FFFFFF] text-[40px] text-glow-strong">80,000,000 VND</span>
                     </div>
-                    <span style={{ width: '625px', right: '50%', top: '50px', zIndex: '50', backgroundColor: '#3D067D', opacity: '0.9', zIndex: '50', fontFamily: 'Brandon Grotesque' }} className="absolute hidden group-hover:block bg-black text-white border-2 border-yellow-500 rounded-md p-5">
+                    <span style={{ width: '720px', right: '50%', top: '50px', zIndex: '50', backgroundColor: '#3D067D', opacity: '0.9', zIndex: '50', fontFamily: 'Brandon Grotesque' }} className="absolute hidden group-hover:block bg-black text-white border-2 border-yellow-500 rounded-md p-5">
                         <div className="w-full flex justify-center">
                             <span className="font-black text-[18px] text-glow-strong">2RD RUNNER UP</span>
                         </div>
-                        <ul className="list-disc text-justify font-light text-[18px]">
-                            <li><p>Internship opportunitiy from MET EV/ person</p></li>
-                            <li><p>1 sampling box including lotion full size 8oz + cream full size 120z from Cerave/person (only for 1 team) --&gt consider passing it on to champion of cerave</p></li>
-                            <li><p>1 3-month membership card from Fonos audiobook/person</p></li>
-                            <li><p>1 21-day experience voucher from Cafornia Fitness & Yoga/person</p></li>
-                            <li><p>Any one full course in the list of 13 courses from Brands Vietnam/person</p></li>
-                            <li><p>1 dental care package worth 1.5 million from Singae Dental/person</p></li>
-                            <li><p>Offer shsortcut to the interview round at the Marketing Trainee position at MVillage</p></li>
+                        <ul className="list-disc text-justify font-light text-[16px] leading-6">
+                            <li><p>1 three-month membership card from Fonos audiobooks per person</p></li>
+                            <li><p>1 21-day experience voucher from California Fitness & Yoga per person</p></li>
+                            <li><p>Study free 1 course from the list of 13 courses from Brands Vietnam per person</p></li>
+                            <li><p>1 dental care package from Singae Dental per person</p></li>
+                            <li><p>1 box of sachet products consisting of Kaya Premium Hainanese and Kaya Premium Nonya per person</p></li>
+                            <li><p>1 internship opportunity from MET EV per person</p></li>
+                            <li><p>1 shortcut offer to interview round for Marketing Trainee position at MVillage per person</p></li>
+                            <li><p>1 shortcut offer to interview round for Executive position at Kaya House per person</p></li>
                         </ul>
                     </span>
                 </div>
@@ -85,17 +96,21 @@ const Awardstructure = () => {
                 <div className='bg-transparent mx-4 group text-headline-31 relative w-[550px] py-10 border-2 border-yellow-500 rounded-md'>
                     <div className="text-center">
                         <PageTitle title={'TOP 8'} className="pb-4"></PageTitle>
-                        <span className="font-bold text-[#FFFFFF] text-[40px] text-glow-strong">100,000,000 VND</span>
+                        <span className="font-bold text-[#FFFFFF] text-[40px] text-glow-strong">50,000,000 VND</span>
                     </div>
-                    <span style={{ width: '625px', left: '50%', top: '50px', zIndex: '50', backgroundColor: '#3D067D', opacity: '0.9', zIndex: '50', fontFamily: 'Brandon Grotesque' }} className="absolute hidden group-hover:block bg-black text-white border-2 border-yellow-500 rounded-md p-5">
+                    <span style={{ width: '720px', left: '50%', top: '50px', zIndex: '50', backgroundColor: '#3D067D', opacity: '0.9', zIndex: '50', fontFamily: 'Brandon Grotesque' }} className="absolute hidden group-hover:block bg-black text-white border-2 border-yellow-500 rounded-md p-5">
                         <div className="w-full flex justify-center">
                             <span className="font-black text-[18px] text-glow-strong">TOP 8</span>
+
                         </div>
-                        <ul className="list-disc text-justify font-light text-[18px]">
-                            <li><p>1 3-month membership card from Fonos audiobook/person</p></li>
-                            <li><p>1 21-day experience voucher from Cafornia Fitness & Yoga/ person</p></li>
-                            <li><p>1 dental care package worth 1.5 million from Singae Dental/person</p></li>
-                            <li><p>Offer shsortcut to the interview round at the Marketing Trainee position at MVillage</p></li>
+                        <ul className="list-disc text-justify font-light text-[16px] leading-6">
+                            <li><p>1 three-month membership card from Fonos audiobooks per person</p></li>
+                            <li><p>1 21-day experience voucher from California Fitness & Yoga per person</p></li>
+                            <li><p>1 dental care package from Singae Dental per person</p></li>
+                            <li><p>1 box of sachet products consisting of Kaya Premium Hainanese and Kaya Premium Nonya per person</p></li>
+                            <li><p>1 shortcut offer to interview round for Marketing Trainee position at MVillage per person</p></li>
+                            <li><p>1 shortcut offer to interview round for Executive position at Kaya House per person</p></li>
+
                         </ul>
                     </span>
                 </div>
