503e32a91e87e57b145b7e98ee8577f0fd1dd813
MarCharWeb <108247146+MarCharWeb@users.noreply.github.com>
2024-03-09 00:12:44 +0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -164,7 +164,7 @@ export default function Home({ data }) {
             <div className='relative flex justify-center -top-16 lg:-top-2 xl:-top-2 sm:top-6 lg:mt-6 mt-20'>
               <Button
                 onClick={() => {
-                  window.location.href = 'https://quickom.net/event/opening-ceremony-marketing-challengers-1564?zarsrc=1303&utm_source=zalo&utm_medium=zalo&utm_campaign=zalo';
+                  // window.location.href = 'https://quickom.net/event/opening-ceremony-marketing-challengers-1564?zarsrc=1303&utm_source=zalo&utm_medium=zalo&utm_campaign=zalo';
                 }}
                 isGlow={true}
                 type='primary'
